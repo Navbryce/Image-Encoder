2f99ac7b36800791c587cb0e29a3547f46d9ccdd
Bryce Plunkett <navbarry@gmail.com>
2018-04-26 17:59:23 -0400

    completed the cli for creating a secret image

--- scripts/user-encode.py
+++ scripts/user-encode.py
@@ -6,6 +6,9 @@ def create_steg():
     image_file_string = get_image_file_string()
     encrypt_image = StegImage(image_type, image_file_string = image_file_string)
     message_parameters = get_message_parameters()
+    encrypt_image.embed_message(message_parameters["message"], message_parameters["secret_key"], message_parameters["random_seed"])
+    encrypt_image.write_bytes_to_image(get_new_image_path())
+
 def get_image_file_string():
     """
     Prompts user for path. Returns image file string (a string representation of a COMPLETE file). will keep prompting until user gives real image
@@ -37,9 +40,12 @@ def get_message_parameters():
     random_seed = input("Enter the steg random seed. This is also used for encryption. The more random, the better: ")
     return {
         "message": message,
-        "encryption_key": encryption_key,
+        "secret_key": encryption_key,
         "random_seed": random_seed
     }
+def get_new_image_path():
+    new_path = input("Enter the path (and the name) to the new image. Include the '.bmp'")
+    return new_path
 
 if __name__ == '__main__': # if someone directly ran this script rather than importing it, run the code below
     create_steg()
