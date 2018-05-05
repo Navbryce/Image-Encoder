41303247b690f437d4f86b1f5ffc5addb8ec3d5a
Bryce Plunkett <irvingwashington01@gmail.com>
2018-05-05 11:03:06 -0400

    Added radio buttons. Fixed some layout problems. Added error handling if the user doesn't select a file.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class ImageEncoder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageEncoder));
+            this.imageUploadDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.picturePreview = new System.Windows.Forms.PictureBox();
+            this.actionSelectPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.encodeActionButton = new System.Windows.Forms.RadioButton();
+            this.decodeActionButton = new System.Windows.Forms.RadioButton();
+            this.parametersPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.requiredParametersPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.messageParameterPanel = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePreview)).BeginInit();
+            this.actionSelectPanel.SuspendLayout();
+            this.parametersPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageUploadDialog
+            // 
+            this.imageUploadDialog.FileName = "imageDialogue";
+            resources.ApplyResources(this.imageUploadDialog, "imageUploadDialog");
+            this.imageUploadDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picturePreview
+            // 
+            this.picturePreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.picturePreview, "picturePreview");
+            this.picturePreview.Name = "picturePreview";
+            this.picturePreview.TabStop = false;
+            this.picturePreview.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // actionSelectPanel
+            // 
+            this.actionSelectPanel.Controls.Add(this.label2);
+            this.actionSelectPanel.Controls.Add(this.encodeActionButton);
+            this.actionSelectPanel.Controls.Add(this.decodeActionButton);
+            resources.ApplyResources(this.actionSelectPanel, "actionSelectPanel");
+            this.actionSelectPanel.Name = "actionSelectPanel";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // encodeActionButton
+            // 
+            resources.ApplyResources(this.encodeActionButton, "encodeActionButton");
+            this.encodeActionButton.Name = "encodeActionButton";
+            this.encodeActionButton.TabStop = true;
+            this.encodeActionButton.UseVisualStyleBackColor = true;
+            this.encodeActionButton.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // decodeActionButton
+            // 
+            resources.ApplyResources(this.decodeActionButton, "decodeActionButton");
+            this.decodeActionButton.Name = "decodeActionButton";
+            this.decodeActionButton.TabStop = true;
+            this.decodeActionButton.UseVisualStyleBackColor = true;
+            this.decodeActionButton.CheckedChanged += new System.EventHandler(this.decodeActionButton_CheckedChanged);
+            // 
+            // parametersPanel
+            // 
+            this.parametersPanel.Controls.Add(this.requiredParametersPanel);
+            this.parametersPanel.Controls.Add(this.messageParameterPanel);
+            resources.ApplyResources(this.parametersPanel, "parametersPanel");
+            this.parametersPanel.Name = "parametersPanel";
+            // 
+            // requiredParametersPanel
+            // 
+            resources.ApplyResources(this.requiredParametersPanel, "requiredParametersPanel");
+            this.requiredParametersPanel.Name = "requiredParametersPanel";
+            // 
+            // messageParameterPanel
+            // 
+            resources.ApplyResources(this.messageParameterPanel, "messageParameterPanel");
+            this.messageParameterPanel.Name = "messageParameterPanel";
+            // 
+            // ImageEncoder
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.parametersPanel);
+            this.Controls.Add(this.actionSelectPanel);
+            this.Controls.Add(this.picturePreview);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ImageEncoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picturePreview)).EndInit();
+            this.actionSelectPanel.ResumeLayout(false);
+            this.actionSelectPanel.PerformLayout();
+            this.parametersPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog imageUploadDialog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picturePreview;
+        private System.Windows.Forms.FlowLayoutPanel actionSelectPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton encodeActionButton;
+        private System.Windows.Forms.RadioButton decodeActionButton;
+        private System.Windows.Forms.FlowLayoutPanel parametersPanel;
+        private System.Windows.Forms.FlowLayoutPanel requiredParametersPanel;
+        private System.Windows.Forms.FlowLayoutPanel messageParameterPanel;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class ImageEncoder : Form
+    {
+        public static int ENCODING = 1;
+        public static int DECODING = -1;
+        public static int NEUTRAL = 0;
+
+        private String defaultDialogueValue;
+
+        private Size maxImagePreviewSize;
+        private int status = ImageEncoder.NEUTRAL; // represents what the user is trying to do
+        public ImageEncoder()
+        {
+            InitializeComponent();
+            maxImagePreviewSize = picturePreview.Size;
+            defaultDialogueValue = imageUploadDialog.FileName;
+
+            // Disable action buttons until an image is selected
+            toggleActionButtons(false);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            imageUploadDialog.Filter = "Image Files(*.BMP) | *.BMP"; // Only allow BMP files
+            imageUploadDialog.ShowDialog();
+
+            String filepath = imageUploadDialog.FileName;
+
+            if (filepath.Length > 0 && !filepath.Equals(defaultDialogueValue)) // returns the defaultDialogueValue if no image was selected
+            {
+                System.Drawing.Image image = System.Drawing.Image.FromFile(filepath);
+                System.Drawing.Image previewImage = (System.Drawing.Image)image.Clone(); // Clones the Image so it can be modified to fit the preview box
+
+                // Resize the piciture box to maintain image dimensions
+                Size imageDimensions = previewImage.Size;
+                Size newImageDimensions = scaleImageToSize(imageDimensions, maxImagePreviewSize); // maxImagePreviewSize set in constructor of form
+                Console.Write(newImageDimensions);
+                // Apply resize and set image
+                picturePreview.Size = newImageDimensions;
+                picturePreview.Image = previewImage;
+
+                // Enable action buttons
+                toggleActionButtons(true);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            updateActionStatus(); // updates the status variable
+        }
+        private void decodeActionButton_CheckedChanged(object sender, EventArgs e)
+        {
+            updateActionStatus(); // updates the status variable
+
+        }
+        // Utility functions
+
+        // Private utility functions
+        private Size scaleImageToSize(Size imageSize, Size boxSize)
+        {
+            double ratio = (double)imageSize.Width / (double)imageSize.Height;
+            int widthBasedOnHeight = (int)(ratio * boxSize.Height); // Use the boxes height?
+
+            Size returnSize;
+            if (widthBasedOnHeight > boxSize.Width) // the width based on the height is too large
+            {
+                int heightBasedOnWidth = (int)(boxSize.Width / ratio); // Yields a height
+                returnSize = new Size(boxSize.Width, heightBasedOnWidth);
+            }
+            else
+            {
+                returnSize = new Size(widthBasedOnHeight, boxSize.Height);
+            }
+            return returnSize;
+        }
+        private void toggleActionButtons(Boolean newStatus)
+        {
+            RadioButton[] radioButtons = { decodeActionButton, encodeActionButton };
+            radioToggle(radioButtons, newStatus);
+        }
+
+        private void radioToggle(RadioButton[] radioButtons, Boolean newStatus)
+        {
+            foreach (RadioButton button in radioButtons)
+            {
+                button.Enabled = newStatus;
+            }
+        }
+        /**
+         * Will update the action status based on buttons.
+         * Returns the OLD status
+         * */
+        private int updateActionStatus()
+        {
+            int oldStatus = status;
+            if (decodeActionButton.Checked)
+            {
+                status = ImageEncoder.DECODING;
+            } else if (encodeActionButton.Checked)
+            {
+                status = ImageEncoder.ENCODING;
+            } else
+            {
+                status = ImageEncoder.NEUTRAL;
+            }
+            return oldStatus;
+        }
+
+
+    }
+}
