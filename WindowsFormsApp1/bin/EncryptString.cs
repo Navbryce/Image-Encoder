3ce174ab19c7019d234a0b04387ecbf36edf7c4d
Bryce Plunkett <irvingwashington01@gmail.com>
2018-05-07 18:28:53 -0400

    Worked on most of the methods (not related to encryption) for the EncryptString class

--- WindowsFormsApp1/bin/EncryptString.cs
+++ WindowsFormsApp1/bin/EncryptString.cs
@@ -1,8 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using WindowsFormsApp1.bin.Utilities;
 
-public class EncryptString
-{
-	public EncryptString()
-	{
-	}
+namespace WindowsFormsApp1.bin {
+
+    public class EncryptString
+    {
+        // Static variables
+        public static int byteSize = 8;
+
+        // Public variables
+        public LinkedList<String> binaryList
+        {
+            get
+            {
+                return binaryList;
+            }
+            set
+            {
+                binaryList = value;
+            }
+        }
+        public LinkedList<Byte> bytes
+        {
+            get
+            {
+                return bytes;
+            }
+            set
+            {
+                bytes = value;
+            }
+        }
+        public String message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+            }
+        }
+        public EncryptString(String messageValue)
+        {
+            message = messageValue;
+            bytes = DataManipulation.convertStringToBytes(message);
+            binaryList = DataManipulation.convertByteListToBinaryList(bytes, byteSize);
+        }
+    }
 }
---
+++ WindowsFormsApp1/bin/Utilities/DataManipulation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.bin.Utilities
+{
+    class DataManipulation
+    {
+        // Small data object functions
+        
+         /**
+         * Assumes the user wants to make an 8-bit byte
+         * 
+         * */
+        public static Byte convertBinaryToByte (String binaryValue)
+        {
+            return Convert.ToByte(binaryValue, 2); 
+        }
+        public static String convertByteToBinary (Byte byteValue, int byteSize)
+        {
+            return Convert.ToString(byteValue, 2).PadLeft(byteSize, '0');
+        }
+
+        // List functions
+
+        /**
+         * ASSUMES binary is 8-bit
+         * */
+        public static LinkedList<Byte> convertBinaryListToByteList (LinkedList<String> binaryList, int byteSize) {
+            LinkedList<Byte> bytes = new LinkedList<byte>();
+            foreach (String binaryString in binaryList)
+            {
+                bytes.AddLast(convertBinaryToByte(binaryString));
+            }
+            return bytes;
+        }
+        public static LinkedList<String> convertByteListToBinaryList (LinkedList<Byte> bytes, int byteSize)
+        {
+            LinkedList<String> binaryList = new LinkedList<string>();
+            foreach (Byte byteObject in bytes)
+            {
+                binaryList.AddLast(convertByteToBinary(byteObject, byteSize));
+            }
+            return binaryList;
+        }
+        public static String convertBytesToString (LinkedList<Byte> bytes)
+        {
+            return System.Text.Encoding.UTF8.GetString(bytes.ToArray(), 0, bytes.Count);
+        }
+        public static LinkedList<Byte> convertStringToBytes (String stringValue)
+        {
+            LinkedList<Byte> bytesList = new LinkedList<byte>();
+            Byte[] bytes = Encoding.ASCII.GetBytes(stringValue);
+            foreach (Byte byteObject in bytes)
+            {
+                try
+                {
+                    bytesList.AddLast(byteObject);
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            return bytesList;
+        }
+
+    }
+}
